980c45245e41679c5fbe85de4255f3f9455c849a
B533P-22\533 <eliot.eliot@hotmail.com>
2018-08-24 16:35:55 -0400

    2e commit

---
+++ Assets/script/ScriptRocket.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptRocket : MonoBehaviour
+{
+    public Rigidbody carRigideBody;
+    [SerializeField] public float ForceFoward = 2000;
+    [SerializeField] public float ForcePosition = 100;
+    // Use this for initialization
+    void Start()
+    {
+        carRigideBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageJumpInput();
+        manageHorizontalInput();
+    }
+
+    private void ManageJumpInput()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            carRigideBody.AddForce(new Vector3(0, ForceFoward, 0));
+        }
+        else if (Input.GetAxis("Jump") < 0)
+        {
+            carRigideBody.AddForce(new Vector3(0, -ForceFoward, 0));
+        }
+
+    }
+    private void manageHorizontalInput()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            carRigideBody.AddForceAtPosition(new Vector3(0, 0, ForcePosition),new Vector3());
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            carRigideBody.AddForceAtPosition(new Vector3(0, 0, -ForcePosition),new Vector3());
+        }
+
+    }
+}
